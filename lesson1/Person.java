6804e5af248773fb4b1331328820554f3de8f714
Petr Vershinin <pitver2014@yandex.ru>
2020-06-08 11:09:33 +0300

    add Person

---
+++ lesson1/Person.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+class Person{
+	private String name;
+	
+	public Person(){};
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+}
